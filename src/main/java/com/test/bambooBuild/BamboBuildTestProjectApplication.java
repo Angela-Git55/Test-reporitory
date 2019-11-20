37086d77c18b8455841ddee28326b2568e56b6ae
Trajche Nikolovski <Trajche.Nikolovski@seavus.com>
2019-11-20 10:03:53 +0100

    Test Intial Bamboo Commit

---
+++ src/main/java/com/test/bambooBuild/BamboBuildTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.test.bambooBuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BamboBuildTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BamboBuildTestProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/bambooBuild/BamboBuildTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.bambooBuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BamboBuildTestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
